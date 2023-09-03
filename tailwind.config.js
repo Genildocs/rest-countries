a4e9ed40ed32606f1c7a0134764b9dd33a4e07ae
genildocs <genildocs@gmail.com>
2023-09-03 05:14:58 -0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,cjs,mjs}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        DarkBlue: "#2b3945",
+        VeryDarkBlue: "#202c37",
+        LightModeText: "#111517",
+        DarkGray: "#858585",
+        VeryLightGray: "#fafafa",
+        whiteText: "#ffffff",
+      },
+    },
   },
   plugins: [],
-}
-
+};
